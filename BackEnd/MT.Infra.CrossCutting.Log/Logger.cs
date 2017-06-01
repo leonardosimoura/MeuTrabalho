c61a3d77e61475f9801cfe65d95a8e9d3c129f0d
Leonardo Silvio de Moura <leonardosimoura@gmail.com>
2017-05-31 22:52:55 -0300

    commit

---
+++ BackEnd/MT.Infra.CrossCutting.Log/Logger.cs
@@ -0,0 +1,17 @@
+﻿using MT.Domain.Shared.Logs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MT.Infra.CrossCutting.Log
+{
+    public class Logger : ILogger
+    {
+        public void AddLog(object data)
+        {
+            //throw new NotImplementedException();
+        }
+    }
+}
